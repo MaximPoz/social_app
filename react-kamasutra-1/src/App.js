c70a4dc87aa531af55726d2a193d3e37de383072
MaximPoz <executioner2126@gmail.com>
2021-09-26 16:04:44 +0900

    15:css 2

--- react-kamasutra-1/src/App.js
+++ react-kamasutra-1/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
+import MyPosts from './components/Profile/MyPosts/MyPosts';
 
 const App = () => { //const App = function(){...}
   return (
