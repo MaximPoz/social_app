fb42a21780fcb981c4f38ac940393399beca3e4c
MaximPoz <executioner2126@gmail.com>
2021-09-27 23:51:28 +0900

    19: npm i react-router-dom -save

--- react-kamasutra-1/src/components/Navbar/Nav.jsx
+++ react-kamasutra-1/src/components/Navbar/Nav.jsx
@@ -5,8 +5,8 @@ import s from  './Nav.module.css';
 
 const Nav = () => {
     return (<nav className={s.nav}>
-        <div className={s.act}><a>Profile</a></div>
-        <div className={s.item}><a>Masseges</a></div>
+        <div className={s.act}><a href="/Profile">Profile</a></div>
+        <div className={s.item}><a href="/Dialogs">Masseges</a></div>
         <div className={s.item}><a>Mews</a></div>
         <div className={s.item}><a>Music</a></div>
         <div className={s.item}><a>Settings</a></div>
