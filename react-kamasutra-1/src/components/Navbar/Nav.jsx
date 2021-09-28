05206b7ff17e476d22240d2e72e74083a1ecb111
MaximPoz <executioner2126@gmail.com>
2021-09-28 18:54:17 +0900

    20:NavLink to=

--- react-kamasutra-1/src/components/Navbar/Nav.jsx
+++ react-kamasutra-1/src/components/Navbar/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from  './Nav.module.css';
 
 
 
 const Nav = () => {
     return (<nav className={s.nav}>
-        <div className={s.act}><a href="/Profile">Profile</a></div>
-        <div className={s.item}><a href="/Dialogs">Masseges</a></div>
-        <div className={s.item}><a href="/News">Mews</a></div>
-        <div className={s.item}><a href="/Music">Music</a></div>
-        <div className={s.item}><a href="/Settings">Settings</a></div>
+        <div className={s.item}><NavLink to="/Profile" activeClassName={s.activeLink}>Profile</NavLink></div>
+        <div className={s.item}><NavLink to="/Dialogs" activeClassName={s.activeLink}>Masseges</NavLink></div>
+        <div className={s.item}><NavLink to="/News" activeClassName={s.activeLink}>Mews</NavLink></div>
+        <div className={s.item}><NavLink to="/Music" activeClassName={s.activeLink}>Music</NavLink></div>
+        <div className={s.item}><NavLink to="/Settings" activeClassName={s.activeLink}>Settings</NavLink></div>
     </nav>
     )
 }
