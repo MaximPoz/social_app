04825e5ab917b6504a959fefd6681208c367b927
MaximPoz <executioner2126@gmail.com>
2021-10-03 17:32:42 +0900

    26:структура проекта UI-BLL + DZ

---
+++ react-kamasutra-1/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './../Dialogs.module.css';
+
+
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id
+    return (
+        <div className={s.dialog}>
+            <NavLink to={path} activeClassName={s.active}> {props.name} </NavLink>
+        </div>
+    )
+}
+
+
+export default DialogItem;
--- react-kamasutra-1/src/components/Dialogs/Dialogs.jsx
+++ react-kamasutra-1/src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
+import Message from './Message/Message';
 
-
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id
-    return (
-        <div className={s.dialog}>
-            <NavLink to={path} activeClassName={s.active}> {props.name} </NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.dialog}> {props.message} </div>
-    )
-}
 
 const Dialogs = (props) => {
 
