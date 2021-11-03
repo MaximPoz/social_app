28d09d7aef8586d87be5eda83247da7be4d9d3c5
MaximPoz <executioner2126@gmail.com>
2021-11-03 17:54:40 +0900

    53 классовая компонента Users (extends React.Component)
    side effect - это что-то, что может повлиять на "чистоту" вашей функции.  Добавили  axios запрос в ф-цию что бы вызывать список с сервера кнопкой.
    Написали первую классовую компоненту Users.

--- react-kamasutra-1/src/components/Users/Users.jsx
+++ react-kamasutra-1/src/components/Users/Users.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
 import styles from './Users.module.css';
-import * as axios from 'axios'; 
+import * as axios from 'axios';
 import flyCat from '../../assets/image/flyCat.png'
 
-let Users = (props) => {
-    if (props.users.length === 0) { //когда Users render'ится длинна массива = 0, по этому выполняется условие, потом при rerender'e длинна !=0 и условие не выполняется (иначе из-за вечного цикла будет ошибка)
+class Users extends React.Component { //расширяем что бы реакт мог взаимодействовать с User
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => { //когда сервак даст ответ затем (then) выполни стрелочную ф-цию
-            props.setUsers(response.data.items) //придёт response у него мы берём data, а там items
-        }); //делаем get запрос и нам вернуться пользователи
+    constructor(props){
+        super(props);
+    //    if (this.props.users.length === 0) { //когда Users render'ится длинна массива = 0, по этому выполняется условие, потом при rerender'e длинна !=0 и условие не выполняется (иначе из-за вечного цикла будет ошибка)
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => { //когда сервак даст ответ затем (then) выполни стрелочную ф-цию
+                    this.props.setUsers(response.data.items) //придёт response у него мы берём data, а там
+                }); //делаем get запрос и нам вернуться пользователи
+        }
+    
 
-      
-    }
+  //  getUsers = () => {}  //объявлям метод
+        
 
-    return <div>
+    render(){  //метод render возвращает JSX
+        return <div>
         {
-        props.users.map(u => <div key={u.id}>
-            <span>
-                <div>
-                    <img src={ u.photos.small != null ? u.photo.small : flyCat } className={styles.usersPhoto} />
-                </div>
-                <div>
-                    {u.followed
-                        ? <button onClick={() => { props.unfollow(u.id) }}>UnFollow</button> //если нажать на кнопку вызови unfollow и передай туда id
-                        : <button onClick={() => { props.follow(u.id) }}>Follow</button>    ////если нажать на кнопку вызови follow и передай туда id
-                    }
-                </div>
-            </span>
-            <span>
+            this.props.users.map(u => <div key={u.id}>
                 <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
+                    <div>
+                        <img src={u.photos.small != null ? u.photo.small : flyCat} className={styles.usersPhoto} />
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => { this.props.unfollow(u.id) }}>UnFollow</button> //если нажать на кнопку вызови unfollow и передай туда id
+                            : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>    ////если нажать на кнопку вызови follow и передай туда id
+                        }
+                    </div>
                 </span>
                 <span>
-                    <div>{"u.location.contry"}</div>
-                    <div>{"u.location.city"}</div>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.contry"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
                 </span>
-            </span>
-        </div>)
-    }
+            </div>)
+        }
     </div>
+    }
 }
 
 export default Users;
