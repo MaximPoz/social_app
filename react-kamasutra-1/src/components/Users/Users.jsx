ddb0ef9de85bfee62bc8acbd0e66216f99465ed7
MaximPoz <executioner2126@gmail.com>
2021-11-08 12:28:42 +0900

    54 componentDidMount
    Узнали что есть метод componentDidMount, он монтирует компоненту с сервера

--- react-kamasutra-1/src/components/Users/Users.jsx
+++ react-kamasutra-1/src/components/Users/Users.jsx
@@ -5,47 +5,41 @@ import flyCat from '../../assets/image/flyCat.png'
 
 class Users extends React.Component { //расширяем что бы реакт мог взаимодействовать с User
 
-    constructor(props){
-        super(props);
-    //    if (this.props.users.length === 0) { //когда Users render'ится длинна массива = 0, по этому выполняется условие, потом при rerender'e длинна !=0 и условие не выполняется (иначе из-за вечного цикла будет ошибка)
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => { //когда сервак даст ответ затем (then) выполни стрелочную ф-цию
-                    this.props.setUsers(response.data.items) //придёт response у него мы берём data, а там
-                }); //делаем get запрос и нам вернуться пользователи
-        }
-    
+    componentDidMount() {  // Происходит монтирование компоненты
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {                          //когда сервак даст ответ затем (then) выполни стрелочную ф-цию
+                this.props.setUsers(response.data.items) //придёт response у него мы берём data, а там
+            });                                         //делаем get запрос и нам вернуться пользователи
+    }
 
-  //  getUsers = () => {}  //объявлям метод
-        
-
-    render(){  //метод render возвращает JSX
+    render() {  //метод render возвращает JSX
         return <div>
-        {
-            this.props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photos.small != null ? u.photo.small : flyCat} className={styles.usersPhoto} />
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => { this.props.unfollow(u.id) }}>UnFollow</button> //если нажать на кнопку вызови unfollow и передай туда id
-                            : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>    ////если нажать на кнопку вызови follow и передай туда id
-                        }
-                    </div>
-                </span>
-                <span>
+            {
+                this.props.users.map(u => <div key={u.id}>
                     <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
+                        <div>
+                            <img src={u.photos.small != null ? u.photo.small : flyCat} className={styles.usersPhoto} />
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>UnFollow</button> //если нажать на кнопку вызови unfollow и передай туда id
+                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>    ////если нажать на кнопку вызови follow и передай туда id
+                            }
+                        </div>
                     </span>
                     <span>
-                        <div>{"u.location.contry"}</div>
-                        <div>{"u.location.city"}</div>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.contry"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
                     </span>
-                </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
     }
 }
 
