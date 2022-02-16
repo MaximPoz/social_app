ec3e4c567726b922e257b2b3a51279444f05b317
MaximPoz <executioner2126@gmail.com>
2022-02-16 23:33:50 +0900

    83: подключаем Resilect
    Решаем проблемы из прошлого урока
    
    "Selector принимает state и возвращает полученное значение(часть state в нашем случае)"
    "Selector инкапсулирует детали"
    "Если у вас есть сложный selector то создавайте его с помощью библиотеки reselect"

--- react-kamasutra-1/src/redux/users-selectors.js
+++ react-kamasutra-1/src/redux/users-selectors.js
@@ -1,6 +1,12 @@
-export const getUsers = (state) => {   //селектор отрабатывает как ссылка в state, берёт от туда то что нужно
+import { createSelector } from "reselect"
+
+ const getUsersSelector = (state) => {   //селектор отрабатывает как ссылка в state, берёт от туда то что нужно
     return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, (users)=> { //передаём селектор getUsersSelector который будет использоваться что бы получить users (каждому сложному селектору нужен приметивный селектор)
+    return users.filter(u=> true);                          //фейковый фильтр
+})
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize
