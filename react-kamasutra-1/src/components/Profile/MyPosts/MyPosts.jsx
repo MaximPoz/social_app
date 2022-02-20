d7570749c27bb8e8fba78160ffd7f86562b087ff
MaximPoz <executioner2126@gmail.com>
2022-02-20 16:14:06 +0900

    88: Pure function (чистя функция)
    Должна быть:
    1. Имьютабельной - не мутировать то что приходит в пропсах (из store внешнего мира)
    2. Return - всегда должна что то возвращать
    3.No side effect - не делать запросы виз компоненты
    4. Детерминированность (предсказуемость) - сколько раз данные не отправляй в функцию, он вернёт один и тот же ответ (при условии что на вход поступают одинаковые данные)

--- react-kamasutra-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kamasutra-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -14,7 +14,10 @@ const MyPosts = React.memo(props => {  //на входе принимает од
 
       console.log("RENDER")
 
-    let postsElements = props.posts.map(p => <Post message={p.message} like={p.likesCount} />); //в качестве "p" у нас придёт строка массива posts (p можно заменить на что угодно)
+    let postsElements = [...props.posts]
+    .reverse()  //перевернуть копию массива (копию что бы не изменить store во внешнем мире)
+    .map(p =>
+       <Post message={p.message} like={p.likesCount} />); //в качестве "p" у нас придёт строка массива posts (p можно заменить на что угодно)
     //и из строки массива как с props мы забераем один из элементов p.message или p.likesCount
     // т.е. .map это цикл который повторяет элемент за элементом из указанного массива.
 
