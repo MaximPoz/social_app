652f35f424872f0ab74d7d5d6741ace67ae77d58
MaximPoz <executioner2126@gmail.com>
2022-02-26 13:45:28 +0900

    91: установка расширения React / Redux DevTools Extension

--- react-kamasutra-1/src/redux/redux-store.js
+++ react-kamasutra-1/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -18,9 +18,9 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;    //для работы расширения Redux DevTools Extension
+const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
- 
+window.__store__ = store;
 
 export default store;
